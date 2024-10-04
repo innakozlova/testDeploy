e065c0e29cecaf569cd8852cbd144e5cfadd2291
innakozlova <lady.inko@gmail.com>
2024-10-04 11:08:09 +0300

    описание функции

---
+++ prg52.py
@@ -0,0 +1,14 @@
+# Функция (encapsulation -инкапсуляция)
+# Описание функции
+def say_hello(name='noname', surname='nos'):
+    """
+
+    :param name:
+    :param surname:
+    :return:
+    """
+    print('Hello,', name, surname)
+
+# вызов
+say_hello('Дмитрий')
+say_hello()
