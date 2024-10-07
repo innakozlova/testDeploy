a43112108945c6d8695de8ee254d3eb74fb3b64a
innakozlova <lady.inko@gmail.com>
2024-10-07 09:52:32 +0300

    Raise

--- prg56.py
+++ prg56.py
@@ -1,8 +1,12 @@
 #Обработка исключений
+# Try (начало обработки) выполняется попытка какого либо действия
+# except (собственно обработчик) исключение обрабатывается
+# finnally программа продолжается с этого места не зависимо от того, было исключение или нет
+# else если не было исключения
 
 def divide():
-    a = input('Введите А')
-    b = input('Введите В')
+    a = input('Введите А:')
+    b = input('Введите В:')
     try:
         temp=float(a)/float(b)
         return temp
@@ -10,5 +14,7 @@ def divide():
         return 'Не делите на ноль'
     except ValueError:
         return 'Нужно вводить только числа'
+    except Exception as exp:
+        return f'Вот такое вот: {exp} '
     return a / b
 print(divide())
