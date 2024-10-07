01fccca246a33dae2df688c24bfa3cfc06b6607e
innakozlova <lady.inko@gmail.com>
2024-10-07 10:06:59 +0300

    Assertion

---
+++ prg59.py
@@ -0,0 +1,10 @@
+# Утверджение (assertion)
+length=3
+
+try:
+    text=input('Введите любой текст:')
+    assert len(text)>length # утверждение
+    print(f' Вы ввели {text}.')
+except AssertionError:
+    print(f'Нет. Ваш текст короче, чем {length} символа')
+
