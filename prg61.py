28908ead963204111dc6cbe99c7eaba72f6a8638
innakozlova <lady.inko@gmail.com>
2024-10-07 12:01:35 +0300

    main

---
+++ prg61.py
@@ -0,0 +1,21 @@
+# Модули
+# import functions as f
+
+from functions import*
+#if __name__ == '__main__': #главный импортируемый модуль
+def main()
+
+
+
+    # Глобальные функции
+    first = 2
+    second = 4
+
+    print(summ(first, second))
+    print(divide(first, second))
+    print(multi(first, second))
+    print(sub(first, second))
+    print(__name__)
+
+if __name__ == '__main__':
+    main()
