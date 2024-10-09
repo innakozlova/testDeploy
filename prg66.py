daf7ba1ca2fc5364dc7c693b1df03264b5bd9418
innakozlova <lady.inko@gmail.com>
2024-10-09 09:33:47 +0300

    magic method2

--- prg66.py
+++ prg66.py
@@ -1,3 +1,6 @@
+#спецметоды
+# полиморфизм
+
 class Square:
     def __init__(self, s=1):
         self.side = s
@@ -8,17 +11,32 @@ class Square:
     def perimeter(self):
         return float (round(self.side * 4), 2)
 
-    def __str__(self):
+    def __str__(self): #строковое представление того, что получилось
         return f'Это квадрат со стороной {self.side}'
 
-    def __eq__(self, other):  #
+    def __eq__(self, other):  # сравнение объектов
         return self.side==other.side
 
     def __gt__(self, other):
         return self.side > other.side
 
-sq1=Square(4)
-sq2=Square(5)
+    def __add__(self, other):
+        return Square(self.side+other.side)
+
+    def __sub__(self, other):
+        return Square(self.side-other.side) # констурирование нового объекта типа Square
+    def __del__(self): # удаление объектов, программа живет, пока не начал работу del
+        print(f'квадрат со стороной {self.side} стёрт')
+
+    def __repr__(self): # для сложных объектов вместо str
+        return f'Квадрат со стороной {self.side}'
+
+sq1=Square(5)
+sq2=Square(4)
+complex_sq = [Square(2), Square(3)] # комплексный объект со сторонами 2 и 3
+print(complex_sq)
+square=sq1-sq2
+print(square)
 print(sq1)
-print(sq1==sq2)
-print(sq1 >=sq2)
+#print(sq1==sq2)
+#print(sq1 >=sq2)
