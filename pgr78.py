46b01b27f8f0b2765474078b6f30934a0febc883
innakozlova <lady.inko@gmail.com>
2024-11-05 10:53:29 +0300

    crud select

---
+++ pgr78.py
@@ -0,0 +1,19 @@
+# CRUD Create Read(Retrieve) Update Delete
+
+#PEP 249 (connection, cursor)
+
+import sqlite3
+#Подключение (connection)
+connection=sqlite3.connect('films_db.sqlite')
+#Создаем курсор
+cursor=connection.cursor()
+
+#исполнение запроса
+result=cursor.execute("""select title from films where year =? and duration >?""", (2010,90)).fetchall()
+
+#select title from films where genre=(select id from genres where title='фантастика')
+
+for item in result:
+    print(item)
+#закрытие соединения с базой
+connection.close()
