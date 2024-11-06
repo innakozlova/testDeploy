88dfa932c09b4253fbd19e552c338568c099e5f4
innakozlova <lady.inko@gmail.com>
2024-11-06 11:44:38 +0300

    Flask intro

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+from flask import Flask, url_for
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Привет, я приложение  Flask!'
+
+#http://localhost:5000/countdown
+@app.route('/countdown')
+def countdown():
+    cl=['Cтарт']
+    cl+=[str(x) for x in reversed(range(11))]
+    cl.append('Финищ')
+    return '<br>'.join(cl)
+
+@app.route('/contacts')
+def contacts():
+    return""" <b>E-mail: </b> inkys@yandex.ru<br> 
+    <a href ='/'> Контакты в Санкт-Петербурге </a>"""
+
+#Статический контент в папке static
+# Все изображения static/images
+#Таблицы стилей static/css
+#Файлы для скачивания
+#Шрифты static/fonts
+#Файлы JS- cценариев static/js
+#Музыка,видео
+# для удобства пользуемся url_for
+@app.route('/img/<num>')
+def show_img(num=''):
+    """
+
+    :param num: по умолчанию строка
+    <int:num> -целое число
+    <float:num> - действительное число
+    <path:num> -строка со слешами для URL
+    <uuid:num> - идентификатор в 16-м представлении
+    :return: путь к картинке
+    """
+    if num !=0:
+        return f"""
+         <h1>Python</h1>
+          <img src="{url_for('static', filename=f'images/python-{num}.jpeg')}">
+          """
+#http://localhost:5000/two-params/Victor/12
+@app.route('/two-params/<username>/<int:number>')
+def two_params_func(username, number):
+    param=100+number
+    return f"""
+   <h1>Пользователь {username}</h1>
+   <h2>Номер в системе {param}</h2> 
+   """
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(port=5000, host='127.0.0.1')
--- old/pgr78.py
+++ old/pgr78.py
@@ -7,11 +7,10 @@ import sqlite3
 connection=sqlite3.connect('films_db.sqlite')
 #Создаем курсор
 cursor=connection.cursor()
-
 #исполнение запроса
-result=cursor.execute("""select title from films where year =? and duration >?""", (2010,90)).fetchall()
-
-#select title from films where genre=(select id from genres where title='фантастика')
+#result=cursor.execute("""select title from films where year =? and duration >?""", (2010,90)).fetchall(), (2010,90)).fetchall()
+result = cursor.execute("""select title from films where genre=(select id from genres where title=?)""",
+                        ('фантастика')).fetchall()
 
 for item in result:
     print(item)
